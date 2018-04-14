a1c136fedbf44598b044f6d1a60e5fc43ee94b8e
idreamshen <idream.shen@gmail.com>
2018-04-14 14:32:10 +0800

    增加命令行参数解析及多种城市

--- tenement/spiders/douban.py
+++ tenement/spiders/douban.py
@@ -6,20 +6,30 @@ from tenement.items import TenementItem
 class DoubanSpider(scrapy.Spider):
     name = "douban"
     allowed_domains = ["www.douban.com"]
-    start_urls = [
-        'https://www.douban.com/group/145219/discussion?start=0',
-        'https://www.douban.com/group/145219/discussion?start=25',
-        'https://www.douban.com/group/145219/discussion?start=50',
-        'https://www.douban.com/group/145219/discussion?start=75',
-        'https://www.douban.com/group/145219/discussion?start=100',
-        'https://www.douban.com/group/145219/discussion?start=125',
-        'https://www.douban.com/group/HZhome/discussion?start=0',
-        'https://www.douban.com/group/HZhome/discussion?start=25',
-        'https://www.douban.com/group/HZhome/discussion?start=50',
-        'https://www.douban.com/group/HZhome/discussion?start=75',
-        'https://www.douban.com/group/HZhome/discussion?start=100',
-        'https://www.douban.com/group/HZhome/discussion?start=125'
-    ]
+    city = 'hangzhou'
+    urls = {
+        'hangzhou': [
+            'https://www.douban.com/group/145219/discussion',
+            'https://www.douban.com/group/HZhome/discussion'
+        ],
+        'shanghai': [
+            'https://www.douban.com/group/shanghaizufang/discussion',
+            'https://www.douban.com/group/homeatshanghai/discussion',
+            'https://www.douban.com/group/shzf/discussion',
+            'https://www.douban.com/group/zufan/discussion'
+        ]
+    }
+
+    def __init__(self, city=None, *args, **kwargs):
+        super(DoubanSpider, self).__init__(*args, **kwargs)
+        print('请求查询城市为', city)
+        if city in self.urls:
+            self.city = city
+        print('最终查询城市为', self.city)
+
+        for url in self.urls[self.city]:
+            for i in range(5):
+                self.start_urls.append(url + "?start=" + str(i * 25))
 
     def parse(self, response):
         for sel in response.xpath('//td[@class="title"]/a'):
